545fd39491e1d3be306cdf5c2f4515f4feb01dad
J Scott Chapman <scott8675309@gmail.com>
2018-05-27 17:16:11 -0600

    First commit

---
+++ app.js
@@ -0,0 +1,54 @@
+//Create a for loop that returns the sum of all of the positive numbers in this array [1, -12, 7, -800, 5, -3]
+let arr = [2, -8, 8, -15, 12];
+//console.log(sum);
+
+//Break this string into an array and print every other word
+let string = 'If I have seen further, it is by standing on the shoulders of giants.';
+//console.log(stringArray);
+
+//using the sentence stored in the string variable, not string array
+//use string.slice() to print the last word in the string
+//console.log(lastWord);
+
+//create a function that accepts three numbers as arguments and
+//prints the average of those three numbers
+//console.log(getAverage(12, 23, 22));
+
+//create a for loop that prints all the numbers between 1 and 66 that 
+//are divisible by 12
+
+//create a for..of loop that prints in all uppercase the name of 
+//anyone's name in the array that starts with a 'r'
+let peopleArray = ['Ronny', 'Bobby', 'Ricky', 'Mike'];
+
+//create a for..of loop that capitalizes all of the vowels in the
+//sentence and then prints the sentence
+let newString = 'Everyday I Have The Blues';
+
+//use filter to store the customers who have an overdue bill
+//then print out those peoples' names
+let customers = [
+    {
+    name: 'Bill',
+    overDue: true
+    }, {
+    name: 'Tammy',
+    overDue: false
+    }, {
+    name: 'DeAndre',
+    overDue: false
+    }, {
+    name: 'Scott',
+    overDue: true
+    }, {
+    name: 'Tina',
+    overDue: true
+    }
+]
+
+//use a for..of loop to print out all the values for the customers
+
+//create a function that checks to see if a word is a palindrome
+//it should work regardless of spaces, case, or punctuation
+//'anna', 'Anna', 'an, na', and 'an, Na' should all be identified
+//as a palindrome by your function
